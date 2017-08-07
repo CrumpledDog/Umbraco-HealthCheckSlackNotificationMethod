938d92aef83ab8460741df3d17216737802c858a
Jeavon <jeavon@crumpled-dog.com>
2017-08-07 15:14:11 +0100

    WIP

---
+++ src/Our.Umbraco.HCSlackNM.Installer/Configurator/Controllers/Configure.js
@@ -0,0 +1,43 @@
+﻿var configApp = angular.module("UFSPLoader", []);
+
+configApp.controller("Loader", function ($scope, $http, $log) {
+    var getDataUrl = "/Umbraco/backoffice/HealthCheckSlackNotificationMethod/Installer/GetParameters";
+    var postDataUrl = "/Umbraco/backoffice/HealthCheckSlackNotificationMethod/Installer/PostParameters";
+
+    $scope.saved = false;
+
+    // Ajax request to controller for data-
+    $http.get(getDataUrl).success(function (data) {
+        $scope.parameters = data;
+    });
+
+    $scope.submitForm = function (e) {
+        e.preventDefault();
+
+        $http.post(postDataUrl, $scope.parameters)
+            .success(function (data) {
+
+                var status;
+                if (typeof data === "string") {
+                    status = JSON.parse(data);
+                } else {
+                    status = data;
+                }
+
+                $scope.status = status;
+
+                if (status !== "ConnectionError") {
+                    $scope.saved = true;
+                }
+
+            });
+    };
+
+    $scope.capitalizeFirstLetter = function (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    $scope.getInputType = function (param) {
+        return param.toUpperCase() === "USEDEFAULTROUTE" || param.toUpperCase() === "USEPRIVATECONTAINER" ? "checkbox" : "text";
+    };
+});
---
+++ src/Our.Umbraco.HCSlackNM.Installer/Enums/InstallerStatus.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Our.Umbraco.HealthCheckSlackNotificationMethod.Installer.Enums
+{
+    /// <summary>
+    /// Provides enumeration of the installer status codes.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum InstallerStatus
+    {
+        /// <summary>
+        /// The install has completed sucessfully.
+        /// </summary>
+        Ok,
+
+        /// <summary>
+        /// Unable to save to the XDT file.
+        /// </summary>
+        SaveXdtError,
+
+        /// <summary>
+        /// Unable to save the configuration value.
+        /// </summary>
+        SaveConfigError
+    }
+}
